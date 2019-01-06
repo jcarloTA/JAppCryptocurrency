de46bb02b6d039cbe42e02c1c0482c9c34eca978
Jancarlo Ticun <jt@somadtech.com>
2019-01-05 19:30:54 -0600

    pagina del autor terminada

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 export default class About extends React.Component {
 
@@ -9,14 +9,45 @@ export default class About extends React.Component {
 
     constructor(props) {
         super(props)
-        console.log('construc home ')
     }
 
     render() {
         return (
-            <View>
-                <Text>Sobre mi</Text>
+            <View style={styles.container}>
+                <View style={styles.containerLabel}>
+                    <Text style={styles.label}>Nombre:</Text>
+                    <Text style={styles.title}>Jancarlo Ticun</Text>
+                </View>
+                <View style={styles.containerLabel}>
+                    <Text style={styles.label}>Email:</Text>
+                    <Text style={styles.title}>janta1696@gmail.com</Text>
+                </View>
+                <View style={styles.containerLabel}>
+                    <Text style={styles.label}>Número de teléfono:</Text>
+                    <Text style={styles.title}>+502 49077846</Text>
+                </View>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 10
+    },
+    containerLabel: {
+        marginBottom: 20,
+        borderWidth: 1,
+        paddingBottom: 5
+    },
+    label: {
+        backgroundColor: '#f4511e',
+        padding: 10,
+        color: '#fff'
+    },
+    title: {
+        fontSize: 15,
+        fontWeight: 'bold',
+        padding: 5
+    }
+})
